da072ae91cac289f2b622b8cf038ba957d2c114d
Varad Rane <77780623+Izor12@users.noreply.github.com>
2024-07-30 14:10:47 +0530

    working quali

--- quali_alldriver.py
+++ quali_alldriver.py
@@ -11,38 +11,90 @@ class quali_two:
         self.driver1 = driver1
         self.driver2 = driver2
         self.session = session
-        self.quali = ff1.get_session(self.session[0],self.session[1],self.session[2])
+        quali = ff1.get_session(self.session[0],self.session[1],self.session[2])
         quali.load(telemetry=True,laps=True)
+        self.quali = quali
         self.laps_driver1 = quali.laps.pick_driver(self.driver1)
         self.laps_driver2 = quali.laps.pick_driver(self.driver2)
-
-    
-    def name_dri1(self):
-        return quali.get_driver(self.driver1)["FullName"]
-    def name_dri2(self):
-        return quali.get_driver(self.driver2)["FullName"]
-    def telemetery_driver1(self):
+        
+        
+    def data_fab(self):
         driver1_fastest = self.laps_driver1.pick_fastest()
         tel_driver1 = driver1_fastest.get_car_data().add_distance()
-        fig, ax = plt.subplots(3)
-        fig.suptitle("Quali {}".format(self.name_dri1()))
-        ax[0].plot(tel_driver1["Distance"],tel_driver1['Speed'], label = quali.get_driver(self.driver1)["FullName"])
-        ax[0].set(ylabel=  "Speed")
-        ax[1].plot(tel_driver1['Distance'], tel_driver1['Throttle'], label=quali.get_driver(self.driver1)["FullName"])
-        ax[1].set(ylabel='Throttle')
-        ax[2].plot(tel_driver1['Distance'], tel_driver1['Brake'], label=quali.get_driver(self.driver1)["FullName"])
-        ax[2].set(ylabel='Brakes') 
-        ax[2].set(xlabel = "Distance")
-    def telemetery_driver2(self):
+        tel_driver1["Time"] =(tel_driver1['Time'].dt.microseconds // 1000 + tel_driver1['Time'].dt.seconds * 1000)/1000
+        
         driver2_fastest = self.laps_driver2.pick_fastest()
         tel_driver2 = driver2_fastest.get_car_data().add_distance()
+        tel_driver2["Time"] =(tel_driver2['Time'].dt.microseconds // 1000 + tel_driver2['Time'].dt.seconds * 1000)/1000
+        return [driver1_fastest,tel_driver1,driver2_fastest,tel_driver2]
+    
+    def name_dri1(self):
+        return self.quali.get_driver(self.driver1)["FullName"]
+    def name_dri2(self):
+        return self.quali.get_driver(self.driver2)["FullName"]
+   
+
+    def telemetery_driver1(self):
+        tel_driver1 = self.data_fab()[1]
         fig, ax = plt.subplots(3)
         fig.suptitle("Quali {}".format(self.name_dri1()))
-        ax[0].plot(tel_driver2["Distance"],tel_driver2['Speed'], label = quali.get_driver(self.driver2)["FullName"])
+        ax[0].plot(tel_driver1["Time"],tel_driver1['Speed'], label = self.quali.get_driver(self.driver1)["FullName"])
+        ax[1].plot(tel_driver1['Time'], tel_driver1['Throttle'], label=self.quali.get_driver(self.driver1)["FullName"])
+        ax[2].plot(tel_driver1['Time'], tel_driver1['Brake'], label=self.quali.get_driver(self.driver1)["FullName"])
+        
+        ax[1].legend(loc = "lower left")
         ax[0].set(ylabel=  "Speed")
-        ax[1].plot(tel_driver2['Distance'], tel_driver2['Throttle'], label=quali.get_driver(self.driver2)["FullName"])
         ax[1].set(ylabel='Throttle')
-        ax[2].plot(tel_driver2['Distance'], tel_driver2['Brake'], label=quali.get_driver(self.driver2)["FullName"])
         ax[2].set(ylabel='Brakes') 
-        ax[2].set(xlabel = "Distance")
-quali_two("RUS","HAM",[2024,12,"Q"]).telemetery_driver1()
+        ax[2].set(xlabel = "Time in Seconds")
+    
+    def telemetery_driver2(self):
+        tel_driver2 = self.data_fab()[3]
+        fig, ax = plt.subplots(3)
+        
+        fig.suptitle("Quali {}".format(self.name_dri2()))
+        ax[0].plot(tel_driver2["Time"],tel_driver2['Speed'], label = self.quali.get_driver(self.driver2)["FullName"])
+        ax[1].plot(tel_driver2['Time'], tel_driver2['Throttle'], label=self.quali.get_driver(self.driver2)["FullName"])
+        ax[2].plot(tel_driver2['Time'], tel_driver2['Brake'], label=self.quali.get_driver(self.driver2)["FullName"])
+
+        ax[1].legend(loc = "lower left")
+        ax[0].set(ylabel=  "Speed")
+        ax[1].set(ylabel='Throttle')
+        ax[2].set(ylabel='Brakes') 
+        ax[2].set(xlabel = "Time in Seconds")
+        
+        
+        
+        
+    #CHANGE\/\/    
+    def compare(self):
+        temp_data = self.data_fab()
+        tel_driver1 = temp_data[1]
+        tel_driver2 = temp_data[3]
+        fig, ax = plt.subplots(3)
+        fig.suptitle("Quali {} VS {}".format(self.driver1,self.driver2))
+        ax[0].plot(tel_driver1["Time"],tel_driver1['Speed'], label = self.quali.get_driver(self.driver1)["FullName"])
+        ax[1].plot(tel_driver1['Time'], tel_driver1['Throttle'], label=self.quali.get_driver(self.driver1)["FullName"])
+        ax[2].plot(tel_driver1['Time'], tel_driver1['Brake'], label=self.quali.get_driver(self.driver1)["FullName"])
+        
+        
+        
+        ax[0].set(ylabel=  "Speed")
+        ax[1].set(ylabel='Throttle')
+        ax[2].set(ylabel='Brakes') 
+        ax[2].set(xlabel = "Time in Seconds")
+        
+        
+        
+        ax[0].plot(tel_driver2["Time"],tel_driver2['Speed'], label = self.quali.get_driver(self.driver2)["FullName"])
+        ax[1].plot(tel_driver2['Time'], tel_driver2['Throttle'], label=self.quali.get_driver(self.driver2)["FullName"])
+        ax[2].plot(tel_driver2['Time'], tel_driver2['Brake'], label=self.quali.get_driver(self.driver2)["FullName"])
+        ax[1].legend(loc = "lower left")
+
+
+spa = quali_two("HAM","VER",[2024,"SPA","R"])
+
+print(spa.quali.event)
+spa.telemetery_driver1()
+spa.telemetery_driver2()
+spa.compare()
